2e6673f8c01cb817da4037e899515b6e2f3f84f4
TusharGen <130532187+TusharGen@users.noreply.github.com>
2023-05-08 11:51:34 +0530

    minor frontend changes

--- client/src/pages/StudentAssignPage.js
+++ client/src/pages/StudentAssignPage.js
@@ -1,23 +1,55 @@
-import { Link } from "react-router-dom";
+// import { CardContent, styled, TextField } from '@mui/material';
 
+import {
+    Card,
+    CardContent,
+    TextField,
+    Grid,
+    Table,
+    Stack,
+    Paper,
+    Avatar,
+    Button,
+    Popover,
+    Checkbox,
+    TableRow,
+    MenuItem,
+    TableBody,
+    TableCell,
+    Container,
+    Typography,
+    IconButton,
+    TableContainer,
+    TablePagination,
+}
+from '@mui/material';
+import Label from '../components/label';
 
+import Iconify from '../components/iconify';
 
-
-const StudentAssignPage = () => {
+export default function TutorAssignPage() {
     return (
-
-        <div >
-
-            <h4>HIII this is student seected tutors </h4>
-
-        </div>
-
+        <Card>
+            <CardContent>
+                <Typography variant="h4" sx={{ mb: 5 }}>
+                    Your Assignments are here!
+                </Typography>
+                {/* <Grid container spacing={2}>
+                    <Grid xs={12} sm={6} item>
+                        <TextField label="Subject" placeholder='Enter Subject' variant='outlined' fullWidth />
+                    </Grid>
+                    <Grid xs={12} sm={6} item>
+                        <TextField label="Topic" placeholder='Enter Topic' variant='outlined' fullWidth />
+                    </Grid>
+                    <Grid xs={12} sm={12} item >
+                    <TextField type='file' variant='outlined' fullWidth/>
+                    </Grid>
+                </Grid> */}
+                
+                {/* <Button sx={{margin:2, alignItems:'left' , backgroundColor:'#a200ff'}} variant="contained" startIcon={<Iconify icon="eva:plus-fill" />} >
+                    Upload
+                </Button> */}
+            </CardContent>
+        </Card>
     );
-
-};
-
-
-
-
-
-export default StudentAssignPage;
+}
