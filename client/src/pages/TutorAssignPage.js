b224d3cdd15faa73fa61eaacaf869a7b5f79a70d
Piyush Kumar <850066525@genpact.com>
2023-05-09 21:16:10 +0530

    Updated TutorAssignPage.js
    Added code to send data received from frontend to backend

--- client/src/pages/TutorAssignPage.js
+++ client/src/pages/TutorAssignPage.js
@@ -1,6 +1,7 @@
 /* eslint-disable prefer-arrow-callback */
 // import { CardContent, styled, TextField } from '@mui/material';
-
+// eslint-disable-next-line import/no-unresolved
+import axios from 'axios';
 import {
     Card,
     CardContent,
@@ -9,53 +10,71 @@ import {
     Button,
     Typography,
     Snackbar,
-    Alert,
+    Alert
+} from '@mui/material';
 
-}
-from '@mui/material';
-
-import React, { useState , forwardRef} from 'react';
-
-import Label from '../components/label';
-
-import Iconify from '../components/iconify';
-
+import React, { useState, forwardRef } from 'react';
+// import axios from 'axios';
 
 export default function TutorAssignPage() {
 
-    const SnackbarAlert = forwardRef(function(props, ref) {
+    const SnackbarAlert = forwardRef(function (props, ref) {
         return <Alert elevation={6} ref={ref} {...props} />;
-      });
-      
-      
+    });
 
-    const [open, setOpen]= useState(false);
-    const handleClose=(event , reason)=>{
-        if(reason==='clickaway')
-        return
+    const [open, setOpen] = useState(false);
+    const [subject, setSubject] = useState('');
+    const [topic, setTopic] = useState('');
+    const [body, setBody] = useState('');
+
+    const handleClose = (event, reason) => {
+        if (reason === 'clickaway')
+            return
         setOpen(false)
     }
+
+    const handleFormSubmit = async (event) => {
+        event.preventDefault();
+
+        try {
+            const response = await axios.post('/api/assignments', { subject, topic, body });
+            console.log(response.data);
+            setOpen(true);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
     return (
         <Card>
             <CardContent>
                 <Typography variant="h4" sx={{ mb: 5 }}>
                     Assignments
                 </Typography>
-                <Grid container spacing={2}>
-                    <Grid xs={12} sm={6} item>
-                        <TextField label="Subject" placeholder='Enter Subject' variant='outlined' fullWidth />
+                <form onSubmit={handleFormSubmit}>
+                    <Grid container spacing={2}>
+                        <Grid xs={12} sm={6} item>
+                            <TextField label="Subject" placeholder='Enter Subject' variant='outlined' fullWidth value={subject} onChange={(event) => setSubject(event.target.value)} />
+                        </Grid>
+                        <Grid xs={12} sm={6} item>
+                            <TextField label="Topic" placeholder='Enter Topic' variant='outlined' fullWidth value={topic} onChange={(event) => setTopic(event.target.value)} />
+                        </Grid>
+                        <Grid xs={12} sm={12} item>
+                            <TextField label="Body" placeholder='Enter body of the assignment' variant='outlined' fullWidth value={body} onChange={(event) => setBody(event.target.value)} />
+                        </Grid>
+                        <Grid xs={12} sm={12} item >
+                            <TextField type='file' variant='outlined' fullWidth />
+                        </Grid>
                     </Grid>
-                    <Grid xs={12} sm={6} item>
-                        <TextField label="Topic" placeholder='Enter Topic' variant='outlined' fullWidth />
-                    </Grid>
-                    <Grid xs={12} sm={12} item>
-                        <TextField label="Body" placeholder='Enter body of the assignment' variant='outlined' fullWidth />
-                    </Grid>
-                    <Grid xs={12} sm={12} item >
-                    <TextField type='file' variant='outlined' fullWidth/>
-                    </Grid>
-                </Grid>
-                
+
+                    {/* <Button
+                        sx={{ margin: 2, alignItems: 'left', backgroundColor: '#a200ff' }}
+                        variant="contained"
+                        type="submit"
+                    >
+                        Upload
+                    </Button> */}
+
                 <Button 
                     sx={{margin:2, alignItems:'left' , backgroundColor:'#a200ff'}} 
                     variant="contained" 
@@ -63,24 +82,18 @@ export default function TutorAssignPage() {
                     onClick={()=>setOpen(true)}  >
                     Upload
                 </Button>
-                {/* <Snackbar
-                    message='Assignment Uploaded successfully!'
-                    autoHideDuration={4000}
-                    open={open}
-                    onClose={handleClose}
-                    anchorOrigin={{
-                        vertical:'bottom',
-                        horizontal:'center'
-                    }}/> */}
+
+
+                </form>
 
                 <Snackbar
-                open={open}
-                autoHideDuration={6000}
-                onClose={handleClose}
-                anchorOrigin={{
-                    vertical:'bottom',
-                    horizontal:'center'
-                }}>
+                    open={open}
+                    autoHideDuration={6000}
+                    onClose={handleClose}
+                    anchorOrigin={{
+                        vertical: 'bottom',
+                        horizontal: 'center'
+                    }}>
                     <SnackbarAlert onClose={handleClose} severity='success'>
                         Assignment uploaded successfully!
                     </SnackbarAlert>
@@ -89,3 +102,5 @@ export default function TutorAssignPage() {
         </Card>
     );
 }
+
+  
