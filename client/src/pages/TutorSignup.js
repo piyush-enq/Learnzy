a245715df6d71387cd31add4aab79423a9ec6cf4
TusharGen <130532187+TusharGen@users.noreply.github.com>
2023-05-12 12:08:25 +0530

    again added green tick for dialog in tutor signup

--- client/src/pages/TutorSignup.js
+++ client/src/pages/TutorSignup.js
@@ -4,7 +4,7 @@ import "./Login.css";
 
 import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Typography } from "@mui/material";
 
-// import CheckCircleIcon from '@mui/icons-material/CheckCircle';
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
 
 
 const TutorSignupPage = () => {
@@ -44,12 +44,13 @@ const TutorSignupPage = () => {
  
 
   const PostData = async (e) => {
-
+    setOpen(true);
+    console.log('dialog opened')
     e.preventDefault();
     const {name,email,password,phone,age}=tutor;
 
     const res = await fetch("/registertutor",{
-
+      
 
       method:"POST",
       headers:{
@@ -106,7 +107,7 @@ const TutorSignupPage = () => {
     </button>
     <Dialog open={open} onClose={handleClose} aria-labelledby='dialog-title' aria-describedby='dialog-description'>
         <DialogTitle id='dialog-title' sx={{ display: 'flex', alignItems: 'center' }}>
-          {/* <CheckCircleIcon style={{ color: 'green'}} /> */}
+          <CheckCircleIcon style={{ color: 'green' , mr:2}} />
           <Typography>
             Thank You!
           </Typography>
